c14fbc8a01439ccab87c96f7db4eda84ee1539bf
Melvin-KJ <melvinofficial2001@gmail.com>
2024-10-13 16:19:34 +0530

    added shuffle functionality to shuffle the order of images on board

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-    if(lockBoard) return;
+  if (lockBoard) return;
+  if (this === firstCard) return;
   this.classList.toggle('flip');
 
   if (!hasFlippedCard) {
@@ -15,7 +16,6 @@ function flipCard() {
 
     return;
   }
-  hasFlippedCard = false;
   secondCard = this;
 
   checkMatch();
@@ -30,15 +30,28 @@ function checkMatch() {
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
 }
 
 function unFlipCards() {
-    lockBoard = true;
+  lockBoard = true;
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-    lockBoard = false;
+    resetBoard();
   }, 1500);
 }
 
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 cards.forEach((card) => card.addEventListener('click', flipCard));
