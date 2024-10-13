447ae7842a45f36483578a528a05b1ce648e407b
Melvin-KJ <melvinofficial2001@gmail.com>
2024-10-13 16:10:33 +0530

    added lock borad functionality

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if(lockBoard) return;
   this.classList.toggle('flip');
 
   if (!hasFlippedCard) {
@@ -31,9 +33,11 @@ function disableCards() {
 }
 
 function unFlipCards() {
+    lockBoard = true;
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
+    lockBoard = false;
   }, 1500);
 }
 
