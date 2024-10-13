9a71b97081e8ed494cb899601db178f4cf00b91f
Melvin-KJ <melvinofficial2001@gmail.com>
2024-10-13 15:10:32 +0530

    added store card and match card functionality

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-const cards = document.querySelectorAll(".memory-card");
+const cards = document.querySelectorAll('.memory-card');
 
-function flipCard(){
-    this.classList.toggle('flip');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  this.classList.toggle('flip');
+
+  if (!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+    console.log(hasFlippedCard, secondCard);
+
+    //do cards match?
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+    } else {
+      //not a match?
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+      }, 1500);
+    }
+  }
 }
 
-cards.forEach( card => card.addEventListener('click', flipCard))
+cards.forEach((card) => card.addEventListener('click', flipCard));
